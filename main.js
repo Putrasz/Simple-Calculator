adbfbe6e154e5dd985400451e9f792c98d1b7e13
Putrasz <hafidhgpf03@gmail.com>
2024-08-21 14:04:47 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var angka1 = document.getElementById("angka1")
+var angka2 = document.getElementById("angka2")
+var hasil = document.getElementById("hasil")
+function hitung(value) {
+    var proses;
+    switch (value) {
+        case "x":
+            proses = parseInt(angka1.value) * parseInt(angka2.value)
+            hasil.innerHTML = proses
+            break
+        case "/":
+            proses = parseInt(angka1.value) / parseInt(angka2.value)
+            hasil.innerHTML = proses
+            break
+        case "+":
+            proses = parseInt(angka1.value) + parseInt(angka2.value)
+            hasil.innerHTML = proses
+            break
+        case "-":
+            proses = parseInt(angka1.value) - parseInt(angka2.value)
+            hasil.innerHTML = proses
+        break
+    }
+}
